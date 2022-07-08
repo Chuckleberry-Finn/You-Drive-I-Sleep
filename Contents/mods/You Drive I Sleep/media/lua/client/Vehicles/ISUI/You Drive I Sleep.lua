906b3e4ea1cd1b84bc797367a17626b6a372c7a2
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-07-08 13:43:50 -0400

    Corrected to avoid issues with other mods

--- Contents/mods/You Drive I Sleep/media/lua/client/Vehicles/ISUI/You Drive I Sleep.lua
+++ Contents/mods/You Drive I Sleep/media/lua/client/Vehicles/ISUI/You Drive I Sleep.lua
@@ -1,6 +1,5 @@
 require "Vehicles/ISUI/ISVehicleMenu"
 
-
 local ISVehicleMenu_onSleep = ISVehicleMenu.onSleep
 ---@param playerObj IsoPlayer|IsoGameCharacter|IsoMovingObject|IsoObject
 function ISVehicleMenu.onSleep(playerObj, vehicle)
@@ -23,57 +22,57 @@ function ISVehicleMenu.showRadialMenu(playerObj)
 
 	ISVehicleMenu_showRadialMenu(playerObj)
 
-	---Checks from vanilla function <compressed>
-	if (UIManager.getSpeedControls() and UIManager.getSpeedControls():getCurrentGameSpeed() == 0) then return end
-	local vehicle = playerObj:getVehicle() if not vehicle then ISVehicleMenu.showRadialMenuOutside(playerObj) return end
+	local vehicle = playerObj:getVehicle()
 	---@type ISRadialMenu|ISPanelJoypad|ISUIElement|ISBaseObject
 	local menu = getPlayerRadialMenu(playerObj:getPlayerNum())
-	if menu:isReallyVisible() then if menu.joyfocus then setJoypadFocus(playerObj:getPlayerNum(), nil) end menu:undisplay() return end
 
-	local sleepSliceIndex
-	for index,slice in pairs(menu.slices) do
-		if slice.text == getText("IGUI_PlayerText_CanNotSleepInMovingCar") then
-			sleepSliceIndex = index
+	if menu then
+
+		local sleepSliceIndex
+		for index,slice in pairs(menu.slices) do
+			if slice.text == getText("IGUI_PlayerText_CanNotSleepInMovingCar") then
+				sleepSliceIndex = index
+			end
 		end
-	end
 
-	if sleepSliceIndex and (not isClient() or getServerOptions():getBoolean("SleepAllowed")) then
-		local newText = ""
+		if sleepSliceIndex and (not isClient() or getServerOptions():getBoolean("SleepAllowed")) then
+			local newText = ""
 
-		if (not isClient() or getServerOptions():getBoolean("SleepAllowed")) then
-			local doSleep = true
-			local sleepNeeded = not isClient() or getServerOptions():getBoolean("SleepNeeded")
+			if (not isClient() or getServerOptions():getBoolean("SleepAllowed")) then
+				local doSleep = true
+				local sleepNeeded = not isClient() or getServerOptions():getBoolean("SleepNeeded")
 
-			if sleepNeeded and (playerObj:getStats():getFatigue() <= 0.3) then
-				newText = "IGUI_Sleep_NotTiredEnough"
-				doSleep = false
+				if sleepNeeded and (playerObj:getStats():getFatigue() <= 0.3) then
+					newText = "IGUI_Sleep_NotTiredEnough"
+					doSleep = false
 
-			elseif vehicle:isDriver(playerObj) and (vehicle:getCurrentSpeedKmHour() > 1 or vehicle:getCurrentSpeedKmHour() < -1) then
-				newText = "IGUI_PlayerText_CanNotSleepInMovingCar"
-				doSleep = false
+				elseif vehicle:isDriver(playerObj) and (vehicle:getCurrentSpeedKmHour() > 1 or vehicle:getCurrentSpeedKmHour() < -1) then
+					newText = "IGUI_PlayerText_CanNotSleepInMovingCar"
+					doSleep = false
 
-			else
-				if playerObj:getSleepingTabletEffect() < 2000 then
+				else
+					if playerObj:getSleepingTabletEffect() < 2000 then
 
-					if playerObj:getMoodles():getMoodleLevel(MoodleType.Pain) >= 2 and playerObj:getStats():getFatigue() <= 0.85 then
-						newText = "ContextMenu_PainNoSleep"
-						doSleep = false
+						if playerObj:getMoodles():getMoodleLevel(MoodleType.Pain) >= 2 and playerObj:getStats():getFatigue() <= 0.85 then
+							newText = "ContextMenu_PainNoSleep"
+							doSleep = false
 
-					elseif playerObj:getMoodles():getMoodleLevel(MoodleType.Panic) >= 1 then
-						newText = "ContextMenu_PanicNoSleep"
-						doSleep = false
+						elseif playerObj:getMoodles():getMoodleLevel(MoodleType.Panic) >= 1 then
+							newText = "ContextMenu_PanicNoSleep"
+							doSleep = false
 
-					elseif sleepNeeded and ((playerObj:getHoursSurvived() - playerObj:getLastHourSleeped()) <= 1) then
-						newText = "ContextMenu_NoSleepTooEarly"
-						doSleep = false
+						elseif sleepNeeded and ((playerObj:getHoursSurvived() - playerObj:getLastHourSleeped()) <= 1) then
+							newText = "ContextMenu_NoSleepTooEarly"
+							doSleep = false
+						end
 					end
 				end
+				if doSleep then
+					newText = "ContextMenu_Sleep"
+					menu.slices[sleepSliceIndex].command = {ISVehicleMenu.onSleep, playerObj, vehicle}
+				end
+				menu:setSliceText(sleepSliceIndex, getText(newText))
 			end
-			if doSleep then
-				newText = "ContextMenu_Sleep"
-				menu.slices[sleepSliceIndex].command = {ISVehicleMenu.onSleep, playerObj, vehicle}
-			end
-			menu:setSliceText(sleepSliceIndex, getText(newText))
 		end
 	end
 end
